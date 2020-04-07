cd91cea22f8bd32d2ab5dae91aa5a91b4321337b
sanjay26 <sanjay@okrasolar.com>
2020-04-07 17:07:42 +0530

    Adding signIn page

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, Switch } from 'react-router-dom';
 import { Home } from "./core/Home";
 import { Signup } from "./user/Signup";
+import { Signin } from "./user/Signin";
 
 const MainRouter = () => (
     <div>
         <Switch>
-            <Route exact path = "/" component={ Home }></Route>
-            <Route exact path = "/Signup" component = {Signup}></Route>
+            <Route exact path = "/" component={ Home } />
+            <Route exact path = "/Signup" component = { Signup } />
+            <Route exact path = "/Signin" component={ Signin } />
         </Switch>
     </div>
 )
