9f2ba4f87f4fb10db37510460a63a7f8491ee6e2
sanjay26 <cmsanjaykrishna@gmail.com>
2020-04-13 01:00:50 +0530

    adding environment variables

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,61 +1,61 @@
-export const signup = user => {
-    return fetch("http://localhost:8080/signup", {
-            method: "POST",
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(user)
-        })
-        .then(response => {
-            return response.json()
-        })
-        .catch(err => console.log(err))
-}
+export const signup = (user) => {
+  return fetch(`${process.env.REACT_APP_API_URL}/signup`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
 
-export const signIn = user => {
-    return fetch("http://localhost:8080/signIn", {
-            method: "POST",
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(user)
-        })
-        .then(response => {
-            return response.json()
-        })
-        .catch(err => console.log(err))
-}
+export const signIn = (user) => {
+  return fetch(`${process.env.REACT_APP_API_URL}/signIn`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
 
 export const authenticate = (jwt, next) => {
-    if(typeof window !== "undefined") {
-        localStorage.setItem("jwt", JSON.stringify(jwt))
-        next()
-    }
-}
+  if (typeof window !== "undefined") {
+    localStorage.setItem("jwt", JSON.stringify(jwt));
+    next();
+  }
+};
 
 export const signout = (next) => {
-    if(typeof window !== "undefined") localStorage.removeItem("jwt")
-    next()
-    return fetch ("http://localhost:8080/signout", {
-        method: "GET"
+  if (typeof window !== "undefined") localStorage.removeItem("jwt");
+  next();
+  return fetch(`${process.env.REACT_APP_API_URL}/signout`, {
+    method: "GET",
+  })
+    .then((response) => {
+      console.log("signout", response);
+      return response.json();
     })
-    .then(response => {
-        console.log('signout', response)
-        return response.json()
-    })
-    .catch(err=>console.log(err))
-}
+    .catch((err) => console.log(err));
+};
 
 export const isAuthenticated = () => {
-    if(typeof window == "undefined"){
-        return false
-    }
+  if (typeof window == "undefined") {
+    return false;
+  }
 
-    if(localStorage.getItem("jwt")) {
-        return JSON.parse(localStorage.getItem("jwt"))
-    } else {
-        return false
-    }
+  if (localStorage.getItem("jwt")) {
+    return JSON.parse(localStorage.getItem("jwt"));
+  } else {
+    return false;
+  }
 };
