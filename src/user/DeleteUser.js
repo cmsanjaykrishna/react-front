e15a050fbc88594b0a54a2ec90588f6fa7085e8e
sanjay26 <cmsanjaykrishna@gmail.com>
2020-04-14 18:26:10 +0530

    added delte functionality and edit profile page

--- src/user/DeleteUser.js
+++ src/user/DeleteUser.js
@@ -1,8 +1,24 @@
 import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import { isAuthenticated, signout } from "../auth";
+import { remove } from "./apiUser";
 
 class DeleteUser extends Component {
+  state = {
+    redirect: false,
+  };
+
   deleteAccount = () => {
-    console.log("delete account");
+    const token = isAuthenticated().token;
+    const userId = this.props.userId;
+    remove(userId, token).then((data) => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        signout(() => console.log("User is deleted."));
+        this.setState({ redirect: true });
+      }
+    });
   };
 
   deleteConfirmed = () => {
@@ -15,6 +31,9 @@ class DeleteUser extends Component {
   };
 
   render() {
+    if (this.state.redirect) {
+      return <Redirect to="/" />;
+    }
     return (
       <button
         onClick={this.deleteConfirmed}
---
+++ src/user/EditProfile.js
@@ -0,0 +1,104 @@
+import React, { Component } from "react";
+import { isAuthenticated } from "../auth";
+import { read } from "./apiUser";
+
+class EditProfile extends Component {
+  constructor() {
+    super();
+    this.state = {
+      id: "",
+      name: "",
+      email: "",
+      password: "",
+    };
+  }
+
+  componentDidMount() {
+    const userId = this.props.match.params.userId;
+    this.init(userId);
+  }
+
+  handleChange = (name) => (event) => {
+    this.setState({ [name]: event.target.value });
+  };
+
+  clickSubmit = (event) => {
+    event.preventDefault();
+    const { name, email, password } = this.state;
+    const user = {
+      name,
+      email,
+      password,
+    };
+    console.log(user);
+    /* signup(user).then((data) => {
+      if (data.error) this.setState({ error: data.error });
+      else
+        this.setState({
+          error: "",
+          name: "",
+          email: "",
+          password: "",
+          open: true,
+        });
+    }); */
+  };
+
+  editProfileForm = (name, email, password) => (
+    <form>
+      <div className="form-group">
+        <label className="text-muted">Name</label>
+        <input
+          onChange={this.handleChange("name")}
+          type="text"
+          className="form-control"
+          value={name}
+        />
+      </div>
+      <div className="form-group">
+        <label className="text-muted">Email</label>
+        <input
+          onChange={this.handleChange("email")}
+          type="email"
+          className="form-control"
+          value={email}
+        />
+      </div>
+      <div className="form-group">
+        <label className="text-muted">Password</label>
+        <input
+          onChange={this.handleChange("password")}
+          type="password"
+          className="form-control"
+          value={password}
+        />
+      </div>
+      <button onClick={this.clickSubmit} className="btn btn-raised btn-primary">
+        Update
+      </button>
+    </form>
+  );
+
+  init = (userId) => {
+    const token = isAuthenticated().token;
+    read(userId, token).then((data) => {
+      if (data.error) {
+        this.setState({ redirectToSignin: true });
+      } else {
+        this.setState({ id: data._id, name: data.name, email: data.email });
+      }
+    });
+  };
+
+  render() {
+    const { name, email, password } = this.state;
+    return (
+      <div className="container">
+        <h2 className="mt-5 mb-5">Edit Profile</h2>
+        {this.editProfileForm(name, email, password)}
+      </div>
+    );
+  }
+}
+
+export default EditProfile;
